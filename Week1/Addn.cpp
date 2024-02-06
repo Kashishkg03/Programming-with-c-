ecca487da7dd461592feb9721be2917169fab4f8
Kashishkg03 <97089600+Kashishkg03@users.noreply.github.com>
2024-02-06 16:13:36 +0000

    Add n numbers by taking user input

---
+++ Week1/Addn.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int n,sum=0,a;
+  cout<<"Enter the range of numbers:"<< endl;
+  cin>>n;
+  cout<<"Enter the numbers:"<< endl;
+  for(int i=0;i<n;i++)
+    {
+    cin >> a;
+    sum = sum + a;
+    }
+  cout<<"Sum of numbers is :"<<sum;
+  return 0;
+}
