409b3faccb5e28ce600dfcfc78bb4f5b9666af8b
Kashishkg03 <97089600+Kashishkg03@users.noreply.github.com>
2024-02-09 21:15:27 +0530

    Update Addn.cpp

--- Week1/Addn.cpp
+++ Week1/Addn.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
   int n,sum=0,a;
-  cout<<"Enter the range of numbers:"<< endl;
+  cout<<"Enter the range of numbers :"<< endl;
   cin>>n;
   cout<<"Enter the numbers:"<< endl;
   for(int i=0;i<n;i++)
